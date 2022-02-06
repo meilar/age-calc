ff9215d4f9f0239de02278862527519c301b969c
Matt Eilar <meilar@gmail.com>
2022-02-06 11:41:39 -0500

    SolarAge constructor adds all planet ages #green

--- __tests__/solar-age.js
+++ __tests__/solar-age.js
@@ -11,5 +11,17 @@ describe('SolarAge', () => {
     test('should create new SolarAge class object with jupiter age property', () => {
     let output = new SolarAge(12);
     expect(output.jupiterAge).toEqual(1);
+  });    
+
+      test('should create new SolarAge class object with all planet age properties', () => {
+    let output = new SolarAge(100);
+    expect(output.mercuryAge).toEqual(414);
+    expect(output.venusAge).toEqual(162);
+    expect(output.earthAge).toEqual(100);
+    expect(output.marsAge).toEqual(53);
+    expect(output.jupiterAge).toEqual(8);
+    expect(output.saturnAge).toEqual(3);
+    expect(output.uranusAge).toEqual(1);
+    expect(output.neptuneAge).toEqual(0);    
   });           
 });
--- src/solar-age.js
+++ src/solar-age.js
@@ -1,6 +1,13 @@
 export default class SolarAge {
   constructor(age) {
-    this.earthDays = age*365
+    this.earthDays = age*365;
+    this.mercuryAge = parseInt(this.earthDays/88);
+    this.venusAge = parseInt(this.earthDays/225);
     this.earthAge = parseInt(this.earthDays/365);
+    this.marsAge = parseInt(this.earthDays/687);
+    this.jupiterAge = parseInt(this.earthDays/4333);
+    this.saturnAge = parseInt(this.earthDays/10759);
+    this.uranusAge = parseInt(this.earthDays/30687);
+    this.neptuneAge = parseInt(this.earthDays/60190);
   }
 }
