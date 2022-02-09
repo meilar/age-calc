f27ad05a7bab47b1a99ec68395828119577e2a73
Matt Eilar <meilar@gmail.com>
2022-02-08 23:40:09 -0800

    build for gh pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,17 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, './')
   },
   devtool: 'eval-source-map',  
   devServer: {                 
     contentBase: './dist'      
   },
   plugins: [
-    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Shape Tracker',
       template: './src/index.html',
