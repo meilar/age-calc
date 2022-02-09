4fa2ed42e4258bb704a5d31160cd2ccaa70c1133
Matt Eilar <meilar@gmail.com>
2022-02-08 21:21:38 -0800

    add results panel

--- src/index.js
+++ src/index.js
@@ -2,27 +2,41 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import { SolarCitizen, SolarYears } from './solar-years.js';
+import { SolarHuman } from './solar-years.js';
 
-$(document).ready(function() {
-  let x = new SolarCitizen(10);
-  let y = new SolarYears(100);
-  console.log(x);
-  console.log(y);
-})
+let user;
+
+function calculateAge() {
+  let userAge = $("#user-age").val();
+  user = new SolarHuman(userAge);
+  user.name = $("#user-name").val();
+}
+
+function showAgeResult() {
+  $("#age-result").removeClass("hidden");
+  $("#earth-name").text(`${user.name}`);
+  $("#earth-age").text(`${user.currentAge}`);
+  $("#earth-days").text(`${user.solarAge.earthDays}`);
+  $("#mars-age").text(`${user.solarAge.marsYear}`);
+  $("#venus-age").text(`${user.solarAge.venusYear}`);
+  $("#merc-age").text(`${user.solarAge.mercuryYear}`);
+  $("#jup-age").text(`${user.solarAge.jupYear}`);
+}
 
 $("#intro-no").on("click", function() {
   $("#intro").addClass("hidden");
   $("#opt-out").removeClass("hidden");
-})
+});
 
 $(".intro-yes").on("click", function() {
   $("#intro").addClass("hidden");
   $("#opt-out").addClass("hidden");
   $("#lifestyle").removeClass("hidden");
-})
+});
 
 $("#form-yes").on("click", function() {
   $("#lifestyle").addClass("hidden");
-  $("#age-result").removeClass("hidden");
-})
+  calculateAge();
+  showAgeResult();
+});
+
